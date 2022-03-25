90dde19f80930c8d41e5426911db548ebcf9ffb5
namphamba170220 <97431789+namphamba170220@users.noreply.github.com>
2022-03-25 17:37:39 +0700

    sua lai login

--- src/pages/Authentication/Login.jsx
+++ src/pages/Authentication/Login.jsx
@@ -1,13 +1,37 @@
-import React from "react";
-import LoginForm from "../../components/LoginForm";
+import React,{useRef} from "react";
+import { Card, Form, Button } from "react-bootstrap";
+
 const Login = () => {
 
- 
-
+    const emailRef = useRef()
+    const passwordRef = useRef()
+    const passwordConfirmRef = useRef()
   return (
-    <div className="login">
-          <LoginForm />
-    </div>
+    <>
+      <Card>
+          <Card.Body>
+              <h2 className="text-center mb-4"> Sign Up</h2>
+              <Form>
+                  <Form.Group id="email">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control type="email" ref={emailRef} required/>
+                  </Form.Group>
+                  <Form.Group id="password">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="email" ref={passwordRef} required/>
+                  </Form.Group>
+                  <Form.Group id="password-confirm">
+                    <Form.Label>Password Confirmation</Form.Label>
+                    <Form.Control type="password" ref={passwordConfirmRef} required/>
+                  </Form.Group>
+                  <Button className="w-100" type="submit">Sign In</Button>
+              </Form>
+          </Card.Body>
+      </Card>
+      <div className="w-100 text-center mt-2">
+          Already have an account? Log In
+      </div>
+    </>
   );
 };
 
